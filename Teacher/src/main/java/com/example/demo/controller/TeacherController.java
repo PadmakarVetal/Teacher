2d3c26835ae3c07d257ff24942d4868587aaaa9c
Padmakar <padmakarvetal@gmail.com>
2025-12-09 10:30:01 +0530

    add

---
+++ Teacher/src/main/java/com/example/demo/controller/TeacherController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.demo.model.Teacher;
+import com.example.demo.service.TeacherService;
+
+public class TeacherController {
+
+	@Autowired
+	private TeacherService ts;
+	
+	@PostMapping("add")
+	public void add(@RequestBody Teacher t) {
+		ts.add(t);
+	}
+	
+	@GetMapping("display")
+	public List<Teacher> display(){
+		return ts.display();
+	}
+	
+	@DeleteMapping("delete = {id}") 
+		public void delete(@PathVariable Integer id) {
+		ts.delete(id);
+	}
+	
+	@PutMapping("update/{id}")
+	public void update(@PathVariable Integer id, @RequestBody Teacher t) {
+		ts.update(id, t);
+	}
+}
---
+++ Teacher/src/main/java/com/example/demo/service/TeacherService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.Teacher;
+
+public interface TeacherService {
+	void add(Teacher t);
+	List<Teacher> display();
+	void delete(Integer id);
+	void update(Integer id,Teacher t);
+}
